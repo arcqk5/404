e69803b388d0346e78ffdfbacb8e95e5f30421bd
arcqk5 <grigoriylafutkin@yandex.ru>
2021-02-17 03:16:45 +0300

    Debug index.html and adding css/js folder

---
+++ js/app.js
@@ -0,0 +1,15 @@
+VANTA.NET({
+  el: "#page",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xd1c2c7,
+  backgroundColor: 0x8860cf,
+  points: 17.00,
+  maxDistance: 23.00,
+  spacing: 19.00
+})
